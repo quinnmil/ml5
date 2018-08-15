0828743cd343e63b46389c5f394fc5ec0408a9fc
mhernan7 <39255541+margielamadman@users.noreply.github.com>
2018-08-14 18:04:08 -0700

    Implement database entry

--- the-system/the-system-app/the-system.py
+++ the-system/the-system-app/the-system.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import url_for
+from flask import redirect
 import pymongo
 from pymongo import MongoClient
 client = MongoClient()
-db = client.test_db
-collection = db.test_collection
+db = client.bad_results_db
+collection = db.bad_results_collection
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -18,7 +20,15 @@ def index():
 def image():
         return render_template('image.html')
 
+@app.route("/results", methods=['POST'])
+def results():
+	label = request.form.get('label')
+	prob = request.form.get('probability')
+	result = {"label": label,
+		"probability":prob}
+	collection.insert(result)
+	return redirect(url_for('image'))
 
 if __name__ == '__main__':
- app.run()
+	app.run()
 
